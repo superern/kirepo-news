e41b4300dfbed555db73afde3d90b4afeda07fc0
Superern <ernandrewgregorio@gmail.com>
2021-07-22 05:12:30 +0800

    add detach tags

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -41,4 +41,9 @@ class Article extends Model
     {
         return $this->tags()->syncWithoutDetaching($tags);
     }
+
+    public function detachTags($tags)
+    {
+        return $this->tags()->detach($tags);
+    }
 }
